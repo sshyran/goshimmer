7846b3fcd8f8b4dd5e13bd330fe94859b44dcfe4
NaitsabesMue <Naitsabes.Relleum@gmail.com>
2021-01-19 11:14:57 +0100

    removed multiple workers

--- tools/spammer/main.go
+++ tools/spammer/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 
 	"github.com/iotaledger/goshimmer/client"
@@ -12,33 +11,30 @@ import (
 )
 
 const (
-	cfgNodeURI      = "node"
-	cfgMessage      = "message"
-	numberOfSpammer = int(1500)
-	spamN           = "spamN"
+	cfgNodeURI = "node"
+	cfgMessage = "message"
+	mps        = "mps"
 )
 
 func init() {
 	flag.String(cfgNodeURI, "http://localhost:8080", "the URI of the node API")
 	flag.String(cfgMessage, "Hello Tangle", "the URI of the node API")
-	flag.Int(spamN, 1000, "number of spammer worker") // this does not work
+	flag.Int(mps, 10000, "mean maximal number of messages per second") // this does not work
 }
 
-func worker(id int, wg *sync.WaitGroup) {
-	defer wg.Done()
+func spam(mps int) {
 	goshimAPI := client.NewGoShimmerAPI(viper.GetString(cfgNodeURI))
 	messageBytes := []byte(viper.GetString(cfgMessage))
 	var issued, failed int
 	for {
-		fmt.Printf("                                                      \r")
-		fmt.Printf("issued %d, failed %d, worker %d\r", issued, failed, id)
+		fmt.Printf("issued %d, failed %d\r", issued, failed)
 		_, err := goshimAPI.Data(messageBytes)
 		if err != nil {
 			failed++
 			continue
 		}
 		issued++
-		time.Sleep(time.Duration(rand.ExpFloat64()) * time.Second)
+		time.Sleep(time.Duration(rand.ExpFloat64()/float64(mps)) * time.Second)
 	}
 }
 
@@ -47,11 +43,5 @@ func main() {
 	if err := viper.BindPFlags(flag.CommandLine); err != nil {
 		panic(err)
 	}
-	var wg sync.WaitGroup
-	numberOfSpammer := viper.GetInt(spamN)
-	for i := 1; i <= numberOfSpammer; i++ {
-		wg.Add(1)
-		go worker(i, &wg)
-	}
-	wg.Wait()
+	spam(viper.GetInt(mps))
 }
